6223bd89958201097fbfe3b7de62b582212761e5
Terts Diepraam <terts.diepraam@gmail.com>
2018-09-13 12:51:22 +0200

    Initial commit

---
+++ clicky.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    let counter = document.getElementById("counter")
+    let plus = document.getElementById("plus")
+    let minus = document.getElementById("minus")
+    plus.addEventListener("click", (event) => {
+        counter.innerHTML = parseInt(counter.innerHTML, 10) + 1
+    })
+    minus.addEventListener("click", (event) => {
+        counter.innerHTML = parseInt(counter.innerHTML, 10) - 1
+    })
+}
